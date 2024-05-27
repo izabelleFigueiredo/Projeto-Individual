cc43e65d14760e68d17cd08e9653572526f513e0
izabelleFigueiredo <izabelle.figueiredo@sptech.school>
2024-05-27 20:18:44 -0300

    att script, ajustes página user

--- Documentos - WanoQuest/Banco de Dados - modelagem e script/scriptBDtabelas.sql
+++ Documentos - WanoQuest/Banco de Dados - modelagem e script/scriptBDtabelas.sql
@@ -74,36 +74,20 @@ insert into personagem (nome) values
 ('Kozuki Hiyori'),
 ('Kurozumi Orochi');
 
--- ("Kin'emon"), ('Denjiro'),('Raizo'),('Shinobu'),('Kawamatsu('Izo('Nekomamushi'),('Kanjuro'),
+-- insert into usuario (nome, email, senha, fkPersonagem) values ('Fernando Alves', 'fernando@outlook.com', md5('ferdARRO1920#'), 1),('Clara Fernandes', 'clara.fernandes@outlook.com', md5('#cald.4509_'), 11);
 
+insert into quiz values (default, 'Onigashima em Chamas', 'Região de Onigashima, questões englobam o ODS 1 - Erradicação da fome', 10);
 
+-- (default, 'Kuri Desumana', 'Região de Kuri, questões englobam o ODS 6 - Diminuição das desigualdades', 14);
 
-insert into usuario (nome, email, senha, fkPersonagem) values 
-('Fernando Alves', 'fernando@outlook.com', md5('ferdARRO1920#'), 1),
-('Clara Fernandes', 'clara.fernandes@outlook.com', md5('#cald.4509_'), 11);
+-- insert into pontuacao values (1, 1, 8);
 
-insert into quiz values 
-(default, 'Onigashima em Chamas', 'Região de Onigashima, questões englobam o ODS 1 - Erradicação da fome', 10),
-(default, 'Kuri Desumana', 'Região de Kuri, questões englobam o ODS 6 - Diminuição das desigualdades', 14);
+insert into artigo (titulo) values ('As Desigualdades em Kuri');
+-- ('O que um Imperador faria pelo One Piece?'),
+-- ('Trabalho Escravo e Indústrias Poluentes');
 
-insert into pontuacao values 
-(1, 1, 8),
-(1, 2, 5),
-(2, 2, 12),
-(2, 1, 7);
+-- insert into curtir values (1, 1),
 
-insert into artigo (titulo) values
-('As Desigualdades em Kuri'),
-('O que um Imperador faria pelo One Piece?'),
-('Trabalho Escravo e Indústrias Poluentes');
-
-insert into curtir values 
-(1, 1),
-(1, 2),
-(1, 3),
-(2, 1),
-(2, 2),
-(2, 3);
 
 
 select usuario.nome,
@@ -128,13 +112,5 @@ join artigo a on fkArtigo = idArtigo;
 SELECT * FROM personagem;
 SELECT * FROM usuario;
 
-
-UPDATE usuario
-SET senha = MD5('123456')
-WHERE idUsuario = 1;
-
 SELECT * FROM usuario WHERE email = 'joao@.com'  AND senha = MD5('122');
 select * from usuario;
-
-
- -- INSERT INTO usuario (nome, email, senha, fkPersonagem) VALUES
