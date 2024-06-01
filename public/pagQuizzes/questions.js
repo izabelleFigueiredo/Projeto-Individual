6d3f66e6c0eb77d506da660989309ace0b33e820
izabelleFigueiredo <izabelle.figueiredo@sptech.school>
2024-06-01 01:20:00 -0300

    conclusao da pagina de tema, modificacoes no menu de tema e quiz

--- public/pagQuizzes/questions.js
+++ public/pagQuizzes/questions.js
@@ -1,63 +1,107 @@
 let questions = [
     {
-        question: 'DESSAS BANDAS, QUAL A SUA FAVORITA?',
+        question: 'Por que a terra de Kuri não é fértil?',
         options: [
-            'THE BEATLES',
-            'NIRVANA',
-            'QUEEN',
-            'SKANK',
-            'ARCTIC MONKEYS',
-            'METALLICA',
-            'BLACK SABBATH',
-            'GUNS N ROSES',
-            'LEGIÃO URBANA',
-            'NENHUMA DESSAS'
+            'Todas das alternativas',
+            'Devido à poluição causada por fábricas instaladas por Kaido',
+            'Por causa de uma seca prolongada causada pelo Zunesha, que toma poucos banhos',
+            'Devido ao clima da região',
+            'Por falta de sementes de qualidade',
+            'Por causa da destruição feita por X Drake mandado por Orochi',
+            'Por que os capangas do shogun Orochi envenenaram a terra',
+            'Nenhuma das alternativas'
         ],
         answer: 1
     },
     {
-        question: 'DESSES INSTRUMENTOS, QUAL O SEU FAVORITO?',
+        question: 'Qual é a única fonte de água disponível para os moradores de Kuri?',
         options: [
-            'VIOLÃO',
-            'BATERIA',
-            'GUITARRA',
-            'BAIXO',
-            'TECLADO',
-            'VOCAL',
-            'NENHUM DESSES'
+            'Água de poços artesianos',
+            'Água mineral importada de outras ilhas',
+            'Água de poços artesianos',
+            'Água do mar',
+            'Água do rio poluída pelos próprios moradores',
+            'Água do rio poluída pelas das fábricas',
+            'Nenhuma das alternativas'
         ],
         answer: 2
     },
     {
-        question: 'DESSAS PERSONALIDADES, QUAL A SUA FAVORITA?',
+        question: 'O que acontece com os moradores que comem as Smiles defeituosas?',
         options: [
-            'ELVIS PRESLEY',
-            'RITA LEE',
-            'CHUCK BERRY',
-            'GEORGE HARRISON',
-            'ROSSETA THARPE',
-            'RAUL SEIXAS',
-            'R.R.MARTIN',
-            'PAUL MCCARTENEY',
-            'NENHUMA DESSAS'
+            'Ganham poderes especiais de Akuma no Mi',
+            'Ficam doentes',
+            'Tornam-se imortais',
+            'Tornam-se extremamente fortes',
+            'Tornam-se subordinados do Kaido',
+            'Ficam felizes e falecem em poucos dias',
+            'Perdem a capacidade de expressar tristeza e só conseguem rir',
+            'Perdem a capacidade de expressar felicidade e só conseguem chorar',
+            'Nenhuma das alternativas'
         ],
         answer: 1
     },
     {
-        question: 'VOCÊ FAZ PARTE DE ALGUMA BANDA?',
+        question: 'Qual personagem é presenteado(a) com um prato de arroz em seu aniversário?',
         options: [
-            'SIM',
-            'NÃO'
+            'Nami',
+            'Toko',
+            'Kiku',
+            'Luffy',
+            'Hiyori',
+            'Tama',
+            'Nenhuma das alternativas'
         ],
         answer: 1
     },
     {
-        question: 'VOCÊ JÁ TINHA CONHECIMENTO SOBRE O UNIVERSO DO ROCK?',
+        question: 'Quais ODS da ONU são notados em relação à situação de Kuri? e a Capital das Flores?',
         options: [
-            'SIM, JÁ CONHECIA TUDO',
-            'SIM, MAS PARCIALMENTE',
-            'NÃO, NUNCA TINHA OUVIDO FALAR'
+            'Todas das alternativas',
+            'Água potável e saneamento e Consumo e Produção Responsáveis',
+            'Erradicação da Pobreza, Fome Zero e Agricultura Sustentável, Redução das Desigualdades',
+            'Redução das Desigualdades e Consumo e Produção Responsáveis',
+            'Redução das Desigualdades, Erradicação da Pobreza, Fome Zero e Agricultura sustentável',
+            'Nenhuma das alternativas'
         ],
         answer: 3
+    },
+    {
+        question: 'O que a reflexão sugere sobre One Piece e sua relevância para o mundo real?',
+        options: [
+            'Todas das alternativas',
+            'É uma série de entretenimento sem relevância social',
+            'É uma obra de arte que provoca reflexões sobre desafios reais',
+            'É uma obra que deve ser assistida apenas por crianças',
+            'Não tem nenhuma relação com problemas do mundo real',
+            'Reflete muitos problemas enfrentados por personagens de outras obras',
+            'Nenhuma das alternativas'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Qual dos seguintes problemas enfrentados pelos moradores de Kuri também é uma realidade para muitos brasileiros?',
+        options: [
+            'Todas das alternativas',
+            'Extrema pobreza',
+            'Falta de acesso a água potável',
+            'Desigualdades sociais e regionais',
+            'Nenhuma das alternativas'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Quem foi o pirata que destruiu as vilas de Kuri?',
+        options: [
+            'Todas das alternativas',
+            'Kaido',
+            'Orochi',
+            'Luffy',
+            'Ace',
+            'Kid',
+            'X Drake',
+            'Nenhuma das alternativas'
+        ],
+        answer: 1
     },
 ];
